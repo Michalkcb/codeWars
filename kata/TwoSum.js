10f4e7b6e4bf84a7072d0e9261c0a441f6ec0484
Michalkcb <michalkcb@gmail.com>
2024-02-02 23:42:00 +0100

    TwoSum

---
+++ kata/TwoSum.js
@@ -0,0 +1,11 @@
+function twoSum(numbers, target) {
+    for (let i = 0; i < numbers.length - 1; i++) {
+      for (let j = i + 1; j < numbers.length; j++) {
+        if (numbers[i] + numbers[j] === target) {
+          return [i, j];
+        }
+      }
+    }
+    // Jeśli nie znaleziono pasującej pary
+    return null;
+  }
