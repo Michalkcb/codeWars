ee1f3fef8abbc8ea3782e6e2044a667be08cc033
Michalkcb <michalkcb@gmail.com>
2024-02-05 19:34:43 +0100

    another lines

--- kata/PersistentBugger.js
+++ kata/PersistentBugger.js
@@ -2,8 +2,11 @@ function persistence(num){
     let persCount = 0;
     whule (num ==="" || num < 1)
         return
-    
-
+    function calc(n){
+        const numStr = n.toString();
+        const digits = numStr.split(' ');
+        
+    }
 
     return persCount;
 }
