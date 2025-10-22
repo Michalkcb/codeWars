add858bc6e1cae5d765f58eef75325e3ba32a2e6
Michał Bany <126069497+Michalkcb@users.noreply.github.com>
2025-10-22 14:44:52 +0200

    Add function to calculate father's age relative to son
    Implement function to calculate when father was twice as old as son.

---
+++ kata/twice_as_old.py
@@ -0,0 +1,10 @@
+'''
+Your function takes two arguments:
+
+current father's age (years)
+current age of his son (years)
+Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old). The answer is always greater or equal to 0, no matter if it was in the past or it is in the future.
+'''
+
+def twice_as_old(dad_years_old, son_years_old):
+    return abs(dad_years_old - 2 * son_years_old)
