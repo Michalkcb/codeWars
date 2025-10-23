6ab7ba1921f67098982acbbd550c29b59621fba0
Michał Bany <126069497+Michalkcb@users.noreply.github.com>
2025-10-23 12:50:02 +0200

    Add to_csv_text function for CSV conversion
    Implement a function to convert a 2D numeric array to CSV format.

---
+++ kata/to_csv_text.py
@@ -0,0 +1,34 @@
+'''
+Create a function that returns the CSV representation of a two-dimensional numeric array.
+
+Example:
+
+input:
+   [[ 0, 1, 2, 3, 4 ],
+    [ 10,11,12,13,14 ],
+    [ 20,21,22,23,24 ],
+    [ 30,31,32,33,34 ]] 
+    
+output:
+     '0,1,2,3,4\n'
+    +'10,11,12,13,14\n'
+    +'20,21,22,23,24\n'
+    +'30,31,32,33,34'
+Array's length > 2.
+
+More details here: https://en.wikipedia.org/wiki/Comma-separated_values
+
+Note: you shouldn't escape the \n, it should work as a new line.
+'''
+def to_csv_text(array):
+    lines = []
+
+    for row in array:
+        str_values = []
+        for value in row:
+            str_values.append(str(value))
+
+        line = ",".join(str_values)
+        lines.append(line)
+
+    return "\n".join(lines)
